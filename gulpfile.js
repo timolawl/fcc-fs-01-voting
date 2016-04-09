c4e86af45233b8e403798aeeb85cd29f059f96cf
Timolawl <timolawl@users.noreply.github.com>
2016-04-09 16:10:33 -0700

    Update gulp development tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
 'use strict';
 
+// Reason for returning gulp.src:
+// http://stackoverflow.com/questions/21699146/gulp-js-task-return-on-src
+
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    gulpif = require('gulp-if'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-cssmin');
+
+var production = process.env.NODE_ENV === 'production';
 
 gulp.task('style', function() {
-    gulp.src('public/stylesheets/*.scss')
+    return gulp.src('app/stylesheets/*.scss')
+        .pipe(plumber())
         .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulpif(production, cssmin())
         .pipe(gulp.dest('public/stylesheets/'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('app/stylesheets/*.scss', ['style']);
+});
+
+gulp.task('default', ['watch', 'style']);
+gulp.task('build', ['style']);
