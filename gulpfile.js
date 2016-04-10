f9fa11105142934052fafa5ee5f20c4ae6c01be2
Timolawl <timolawl@users.noreply.github.com>
2016-04-10 01:13:05 -0700

    Update tasks to run.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 // Reason for returning gulp.src:
 // http://stackoverflow.com/questions/21699146/gulp-js-task-return-on-src
 
+var browserSync = require('browser-sync').create();
+
 var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     sass = require('gulp-sass'),
@@ -12,18 +14,31 @@ var gulp = require('gulp'),
 
 var production = process.env.NODE_ENV === 'production';
 
-gulp.task('style', function() {
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('sass', function () {
     return gulp.src('app/stylesheets/*.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(gulpif(production, cssmin())
-        .pipe(gulp.dest('public/stylesheets/'));
+        .pipe(gulpif(production, cssmin()))
+        .pipe(gulp.dest('public/stylesheets/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/stylesheets/*.scss', ['style']);
+gulp.task('watch', ['browserSync', 'sass'],  function () {
+    gulp.watch('app/stylesheets/*.scss', ['sass']);
+    gulp.watch('public/*.html', browserSync.reload);
+    gulp.watch('public/js/**/*.js', browserSync.reload);
 });
 
-gulp.task('default', ['watch', 'style']);
-gulp.task('build', ['style']);
+gulp.task('default', ['watch', 'sass']);
+gulp.task('build', ['sass']);
